ca00ff8cc7ef7054f33fd141a5cbf50d579e7170
Isaac Shapira <fresheyeball@gmail.com>
2015-01-21 19:47:46 -0700

    types from the README

--- src/Render/Dom/JTable/JTable.purs
+++ src/Render/Dom/JTable/JTable.purs
@@ -2,3 +2,26 @@ module Render.Dom.JTable where
 
 import Data.Argonaut
 import Data.Either
+
+data JSemantic = Integral
+               | Fractional
+               | Date
+               | DateTime
+               | Time
+               | Interval
+               | Text
+               | Bool
+               | Percent
+               | Currency
+               | NA
+
+type Level = Number
+
+data ColumnOrdering = InOrdering | CustomOrdering (JPath -> JPath -> Ordering)
+
+data TableStyle =
+TableStyle {
+  table   :: Level -> Markup -> Markup,
+  cell    :: JSemantic -> Markup -> Markup,
+  head    :: JPath -> Markup -> Markup,
+  row     :: Markup -> Markup }
